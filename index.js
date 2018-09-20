261891364e21642c88e7ef38c77911d458f3916d
Bryan Hostetler <bryan.hostetler@gmail.com>
2018-09-20 04:21:22 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log('hello'.toUpperCase())
+}
+function logWhisper(string) {
+  console.log('hello'.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    console.log('I can\'t hear you!')
+  }
+}
